39b23db1b3c1f863aae3c61a8fdaa57a2e137e96
PlayerFar <110730625+PlayerFar@users.noreply.github.com>
2024-07-21 15:15:53 +0700

    Create PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,7 @@
+import random
+char0 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = [1,2,3,4,5,6,7,8,9]
+char1 = list(char0)
+char = num + char1
+print("Your Password:   ")
+print(random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char),random.choice(char))
